782dab2e7d7112ded0ef7cb30036f6e4d8cc1cc1
Alex <zemzikova.oleksandra@gmail.com>
2025-01-14 10:58:29 +0200

    minus hello world

--- src/server.js
+++ src/server.js
@@ -20,27 +20,6 @@ app.use(
   }),
 );
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello, World!',
-  });
-});
-
-app.use('*', (req, res, next) => {
-  res.status(404).json({ message: 'Not found' });
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 app.get('/contacts', async (req, res) => {
   const contacts = await getAllContacts();
   res.status(200).json({ data: contacts });
@@ -59,4 +38,19 @@ app.get('/contacts/:contactId', async (req, res) => {
   res.status(200).json({
     data: contact,
   });
+});
+
+app.use('*', (req, res, next) => {
+  res.status(404).json({ message: 'Not found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
