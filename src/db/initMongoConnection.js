e98737811264cf8f347def94774508d10fe576d0
Alex <zemzikova.oleksandra@gmail.com>
2025-01-14 11:27:56 +0200

    db2

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,15 +1,12 @@
 import mongoose from 'mongoose';
 import { getEnvVar } from '../utils/getEnvVar.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export const initMongoConnection = async () => {
   try {
-    const user = getEnvVar(process.env.MONGODB_USER);
-    const pwd = getEnvVar(process.env.MONGODB_PASSWORD);
-    const url = getEnvVar(process.env.MONGODB_URL);
-    const db = getEnvVar(process.env.MONGODB_DB);
+    const user = getEnvVar('MONGODB_USER');
+    const pwd = getEnvVar('MONGODB_PASSWORD');
+    const url = getEnvVar('MONGODB_URL');
+    const db = getEnvVar('MONGODB_DB');
 
     await mongoose.connect(
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
