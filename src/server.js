eef193b9614adf741b4248eae81b21d7bdb2917e
Alex <zemzikova.oleksandra@gmail.com>
2025-02-03 15:22:22 +0200

    problem on server

--- src/server.js
+++ src/server.js
@@ -16,22 +16,22 @@ export const setupServer = () => {
     }),
   );
   app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(contactsRouter);
+
+  app.use('*', notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
-
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
-
-app.use(contactsRouter);
-
-app.use('*', notFoundHandler);
-
-app.use(errorHandler);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
