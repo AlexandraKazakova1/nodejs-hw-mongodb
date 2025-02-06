7479da9aeb4b80aa6dbba10ee3b4a525e33b857d
Alex <zemzikova.oleksandra@gmail.com>
2025-02-06 10:15:41 +0200

    authorization

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import contactsRouter from './routers/contacts.js';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import router from './routers/index.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 const app = express();
@@ -25,7 +25,7 @@ export const setupServer = () => {
     }),
   );
 
-  app.use(contactsRouter);
+  app.use(router);
 
   app.use('*', notFoundHandler);
 
